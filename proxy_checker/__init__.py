3d1fbe0891e04c6cb5670f93445c8dbf79e5b860
ProxyStats <hello@proxystats.io>
2026-04-15 12:57:22 +0300

    chore: bump version to 1.0.1

--- proxy_checker/__init__.py
+++ proxy_checker/__init__.py
@@ -7,5 +7,5 @@ Supports HTTP/HTTPS/SOCKS4/SOCKS5.
 from .checker import check_proxies, check_proxy
 from .models import AnonymityLevel, ProxyResult, ProxyType
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["check_proxy", "check_proxies", "ProxyResult", "ProxyType", "AnonymityLevel"]
